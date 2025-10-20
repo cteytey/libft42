9cb5b4dd3dcc9592059a638f0a59644b3f158636
cteytey <dehon.jules1@gmail.com>
2025-10-20 18:22:51 +0200

    Add files via upload

--- ft_split.c
+++ ft_split.c
@@ -6,18 +6,11 @@
 /*   By: judehon <judehon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 14:05:54 by judehon           #+#    #+#             */
-/*   Updated: 2025/10/20 16:40:45 by judehon          ###   ########.fr       */
+/*   Updated: 2025/10/20 18:21:33 by judehon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static int	ft_is_sep(char c, char sep)
-{
-	if (sep == c)
-		return (1);
-	return (0);
-}
 
 static void	ft_free_all(char **s)
 {
@@ -36,67 +29,79 @@ static void	ft_free_all(char **s)
 
 static int	ft_count_words(char const *s, char sep)
 {
-	int	count;
 	int	i;
+	int	count;
 
-	count = 0;
 	i = 0;
+	count = 0;
 	while (s[i])
 	{
-		while (s[i] && ft_is_sep(s[i], sep))
-			i++;
-		if (s[i])
-		{
+		if ((s[i] != sep) && (i == 0 || s[i - 1] == sep))
 			count++;
-			while (s[i] && !ft_is_sep(s[i], sep))
-				i++;
-		}
+		i++;
 	}
 	return (count);
 }
 
-static char	*ft_strduprange(char const *s, int min, int max)
+static char	*ft_worddup(char const *src, char c)
 {
-	char	*dest;
 	int		i;
+	int		len;
+	char	*dest;
 
-	i = 0;
-	dest = malloc(sizeof(char) * (max - min + 1));
+	len = 0;
+	while (src[len] && src[len] != c)
+		len++;
+	dest = malloc(sizeof(char) * (len + 1));
 	if (!dest)
-		return (0);
-	while (i < max - min)
+		return (NULL);
+	i = 0;
+	while (src[i] && src[i] != c)
 	{
-		dest[i] = s[min + i];
+		dest[i] = src[i];
 		i++;
 	}
 	dest[i] = '\0';
 	return (dest);
 }
 
+static int	ft_fill_split(char **new, char const *s, char c)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		if ((s[i] != c) && (i == 0 || s[i - 1] == c))
+		{
+			new[j] = ft_worddup(&s[i], c);
+			if (!new[j])
+			{
+				ft_free_all(new);
+				return (0);
+			}
+			j++;
+		}
+		i++;
+	}
+	new[j] = NULL;
+	return (1);
+}
+
+
 char	**ft_split(char const *s, char c)
 {
 	char	**new;
-	int		i;
-	int		j;
-	int		start;
 
 	if (!s)
 		return (NULL);
 	new = malloc(sizeof(char *) * (ft_count_words(s, c) + 1));
 	if (!new)
 		return (NULL);
-	i = 0;
-	j = 0;
-	while (s[i])
-	{
-		while (s[i] && ft_is_sep(s[i], c))
-			i++;
-		start = i;
-		while (s[i] && !ft_is_sep(s[i], c))
-			i++;
-		if (start < i && !(new[j++] = ft_strduprange(s, start, i)))
-			return (ft_free_all(new), NULL);
-	}
-	new[j] = NULL;
+	if (!ft_fill_split(new, s, c))
+		return (NULL);
 	return (new);
 }
+
